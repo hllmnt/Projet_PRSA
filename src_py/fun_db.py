75e8e1b972f5a179e0e32a82a8015226e63f60fd
elouan.lesteven <elouan.lesteven@ensiie.eu>
2024-04-23 16:50:09 +0200

    added definition of functions necessary to access the database

---
+++ src_py/fun_db.py
@@ -0,0 +1,36 @@
+import pymongo
+import numpy as np
+import pickle
+import json
+
+def toBin(x):
+    return pickle.dumps(x)
+
+def fromBin(x):
+    return pickle.loads(x)
+
+def check_json(json_,database):
+
+    return #TODO
+
+def insert_json(json_,identifier,database):
+
+    return #TODO
+
+def check_potential(identifier,database):
+
+    return #TODO
+
+def check_last_mat(identifier,database):
+
+    return #TODO
+
+def insert_mat(matrix,identifier,iteration,database):
+
+    return #TODO
+
+def insert_potential(matrix,identifier,database):
+
+    return #TODO
+
+
