27c1f3791db39b8686e325c9c980f1130dce682d
hugo.lallemant <hugo.lallemant@ensiie.eu>
2024-05-20 16:51:28 +0200

    functions to get the id of the last step of a job, to get the id of the last job, and to create a job

--- src_py/fun_db.py
+++ src_py/fun_db.py
@@ -3,11 +3,42 @@ import numpy as np
 import pickle
 import json
 
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
 def toBin(x):
     return pickle.dumps(x)
 
 def fromBin(x):
     return pickle.loads(x)
+
+def lastStep(runID):
+    '''Get the value lastStepID from collection JSON_COLLECTION'''
+    db = client["PRSA"]
+    col = db["JSON_COLLECTION"]
+    query = {"runID":runID}
+    jlist = col.find(query)
+    if len(jlist) == 0:
+        return -1
+    return jlist[0]["lastStepID"]
+
+def lastRunID():
+    '''Get the last runID from collection JSON_COLLECTION'''
+    db = client["PRSA"]
+    col = db["JSON_COLLECTION"]
+    jlist = col.find().sort("runID", -1)
+    if jlist.count() == 0:
+        return 0
+    return jlist[0]["runID"]
+
+def createRun(jsonParams):
+    '''Insert a new run in the collection JSON_COLLECTION'''
+    db = client["PRSA"]
+    col = db["JSON_COLLECTION"]
+    runID = lastRunID() + 1
+    x = toBin(jsonParams)
+    col.insert_one({"runID":runID, "json":x, "lastStepID":0})
+    return True
+
 
 def check_json(json_,database):
 
