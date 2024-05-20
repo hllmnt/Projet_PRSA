7e6ccc55efca61c357597d71d435ce555556490a
elouan.lesteven <elouan.lesteven@ensiie.eu>
2024-05-20 18:11:48 +0200

    changes to database access functions

--- src_py/fun_db.py
+++ src_py/fun_db.py
@@ -4,18 +4,19 @@ import pickle
 import json
 import hashlib
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = pymongo.MongoClient("mongodb://localhost:27017/")["PRSA"]
 
 def toBin(x):
+    '''Returns the data stored in x in a binary form'''
     return pickle.dumps(x)
 
 
 def fromBin(x):
+    '''Returns the data stored in binary form in x in its original form'''
     return pickle.loads(x)
 
-def lastStep(runID):
+def lastStep(runID,db):
     '''Get the value lastStepID from collection JSON_COLLECTION'''
-    db = client["PRSA"]
     col = db["JSON_COLLECTION"]
     query = {"runID":runID}
     jlist = col.find(query)
@@ -23,72 +24,43 @@ def lastStep(runID):
         return -1
     return jlist[0]["lastStepID"]
 
-def lastRunID():
+def lastRunID(db):
     '''Get the last runID from collection JSON_COLLECTION'''
-    db = client["PRSA"]
     col = db["JSON_COLLECTION"]
     jlist = col.find().sort("runID", -1)
     if jlist.count() == 0:
         return 0
     return jlist[0]["runID"]
 
-def createRun(jsonParams):
+def createRun(jsonParams,db):
     '''Insert a new run in the collection JSON_COLLECTION'''
-    db = client["PRSA"]
     col = db["JSON_COLLECTION"]
     runID = lastRunID() + 1
     x = toBin(jsonParams)
     col.insert_one({"runID":runID, "json":x, "lastStepID":0})
-    return True
+    return runID
 
-
-def check_json(json_,database):
-
-    json=toBin(json_)
-    col=database["json"]
-    query={"json":json}
-    jlist=col.find(query)
-    len=0
-    for i in jlist:
-        len+=1
-    if(len==1):
-        return True
-    return False
-
-
-def insert_json(json_,identifier,database):
-    
-    json=toBin(json_)
-    col=database["json"]
-    #identifier=hashlib.sha256(json).hexdigest()
-    
-    col.insert_one({"json":json, "id":identifier})
-    return 
-
-
-def check_last_mat(identifier,database):
-
-    col=database["matrix"]
-    query={"id":identifier}
-    matlist=col.find(query)
+def check_last_mat(identifier,db):
+    '''checks what was the last iteration in a run and returns its number'''
+    col=db[identifier]
+    matlist=col.find({})
     last_occurence=-2
     for i in matlist:
-        if (i["itertion"]>last_occurence):
-            last_occurence=i["itertion"]
+        if (i["iteration"]>last_occurence):
+            last_occurence=i["iteration"]
     return last_occurence
 
 
-def insert_mat(matrix_,identifier,iteration,database):
-
-    col=database["matrix"]
+def insert_mat(matrix_,norm,iteration,identifier,db):
+    '''Inserts the matrix in the database'''
+    col=db[identifier]
     matrix=toBin(matrix_)
-    col.insert_one({"matrix":matrix, "id":identifier, "itertion":iteration})
-
+    col.insert_one({"matrix":matrix, "iteration":iteration, "norm":norm})
     return
 
 
-def insert_potential(potential,identifier,database):
-
-    insert_mat(potential,identifier,-1,database)
+def insert_potential(potential,norm,identifier,db):
+    '''Inserts a potentials matrix into the database'''
+    insert_mat(potential,norm,-1,identifier,db)
 
     return
